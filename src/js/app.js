330ea3e69cea33e71a5035612a796e636f5c413b
nikolay <kolunya0@gmail.com>
2024-05-02 20:20:17 +0300

    task 7

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,28 @@
-// TODO: write code here
+/* eslint-disable no-console */
+import HelpDeskControl from './HelpDeskControl';
+import HelpDeskDOM from './HelpDeskDOM';
+import HelpDeskAPI from './HelpDeskAPI';
 
-const subscribeWidget = document.querySelector('[data-widget=subscribe]');
-const subscribeForm = subscribeWidget.querySelector('[data-id=subscribe-form]');
-const nameInput = subscribeWidget.querySelector('[data-id=name]');
-const phoneInput = subscribeWidget.querySelector('[data-id=phone]');
+// элемент блока div в DOM
+const hw = document.querySelector('#hw');
 
-// POST (slide 38)
-subscribeForm.addEventListener('submit', (evt) => {
-	evt.preventDefault();
-	const params = Array.from(subscribeForm.elements)
-		.filter(({name}) => name)
-		.map(({name, value}) => `${name}=${encodeURIComponent(value)}`)
-		.join('&');
-	const url = 'http://localhost:7070';
-	const xhr = new XMLHttpRequest();
-	xhr.open('POST', url, true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	// event listener here
-	xhr.send(params);
-});
+// создание класса отвечающего за DOM
+const helpDeskDOM = new HelpDeskDOM();
+
+// присвоению блока DIV, класса отвечающего за DOM
+helpDeskDOM.bindToDOM(hw);
+
+// ссылка на baсkend
+// const baseURL = 'http://localhost:7070';
+const baseURL = 'https://ahj-hw7-1.onrender.com/';
+
+// создание класса отвечающего за API и присвоение ему ссылки на bakend
+const helpDeskAPI = new HelpDeskAPI(baseURL);
+
+// создание класса отвечающего за контроль
+const helpDeskControll = new HelpDeskControl(helpDeskDOM, helpDeskAPI);
+
+// запуск метода инициализирующий страт
+helpDeskControll.init();
+
+console.log('app started');
--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,15 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  entry: './src/index.js', // входной файл
   target: 'web',
-  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '/dist'),
-    open: true,
-    compress: true,
+    assetModuleFilename: '[name][ext][query]', // add this line
+    // publicPath: '/', // эта штука, что бы менять путь
+    clean: true, // для очистки папки dist при новом билде
   },
   module: {
     rules: [
@@ -38,17 +34,35 @@ module.exports = {
           MiniCssExtractPlugin.loader, 'css-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ogg|mp3|wav)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 40 * 1024, // 40kb
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 70 * 1024, // 70kb
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+      favicon: './src/img/icon.png',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ],
 };
